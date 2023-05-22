743dfb4fc992a19ccf54991c32dcc9fde435a785
totarochristian <totaro.christian.18@gmail.com>
2023-05-22 14:40:37 +0200

    Add menuData file in the config dir

---
+++ config/menuData.php
@@ -0,0 +1,216 @@
+<?php
+return [
+    "menuData" => [
+        [
+            "isActive" => false,
+            "text" => 'characters'
+        ],
+        [
+            "isActive" => true,
+            "text" => 'comics'
+        ],
+        [
+            "isActive" => false,
+            "text" => 'movies'
+        ],
+        [
+            "isActive" => false,
+            "text" => 'tv'
+        ],
+        [
+            "isActive" => false,
+            "text" => 'games'
+        ],
+        [
+            "isActive" => false,
+            "text" => 'collectibles'
+        ],
+        [
+            "isActive" => false,
+            "text" => 'videos'
+        ],
+        [
+            "isActive" => false,
+            "text" => 'fans'
+        ],
+        [
+            "isActive" => false,
+            "text" => 'news'
+        ],
+        [
+            "isActive" => false,
+            "text" => 'shop'
+        ]
+    ],
+    "brandsData" => [
+        [
+            "imageName" => "buy-comics-digital-comics.png",
+            "text" => "digital comics"
+        ],
+        [
+            "imageName" => "buy-comics-merchandise.png",
+            "text" => "dc merchandise"
+        ],
+        [
+            "imageName" => "buy-comics-subscriptions.png",
+            "text" => "subscription"
+        ],
+        [
+            "imageName" => "buy-comics-shop-locator.png",
+            "text" => "comic shop locator"
+        ],
+        [
+            "imageName" => "buy-dc-power-visa.svg",
+            "text" => "dc power visa"
+        ]
+    ],
+    "sitesData" => [
+        [
+            "title" => "dc comics",
+            "links" => [
+                [
+                    "text" => "characters",
+                    "link" => "#"
+                ],
+                [
+                    "text" => "comics",
+                    "link" => "#"
+                ],
+                [
+                    "text" => "movies",
+                    "link" => "#"
+                ],
+                [
+                    "text" => "TV",
+                    "link" => "#"
+                ],
+                [
+                    "text" => "games",
+                    "link" => "#"
+                ],
+                [
+                    "text" => "videos",
+                    "link" => "#"
+                ],
+                [
+                    "text" => "news",
+                    "link" => "#"
+                ]
+            ]
+        ],
+        [
+            "title" => "dc",
+            "links" => [
+                [
+                    "text" => "terms of use",
+                    "link" => "#"
+                ],
+                [
+                    "text" => "privacy-policy (new)",
+                    "link" => "#"
+                ],
+                [
+                    "text" => "ad choices",
+                    "link" => "#"
+                ],
+                [
+                    "text" => "advertising",
+                    "link" => "#"
+                ],
+                [
+                    "text" => "jobs",
+                    "link" => "#"
+                ],
+                [
+                    "text" => "subscriptions",
+                    "link" => "#"
+                ],
+                [
+                    "text" => "talent workshops",
+                    "link" => "#"
+                ],
+                [
+                    "text" => "CPSC certificates",
+                    "link" => "#"
+                ],
+                [
+                    "text" => "ratings",
+                    "link" => "#"
+                ],
+                [
+                    "text" => "shop help",
+                    "link" => "#"
+                ],
+                [
+                    "text" => "contacts us",
+                    "link" => "#"
+                ]
+            ]
+        ],
+        [
+            "title" => "sites",
+            "links" => [
+                [
+                    "text" => "DC",
+                    "link" => "#"
+                ],
+                [
+                    "text" => "MAD magazine",
+                    "link" => "#"
+                ],
+                [
+                    "text" => "DC kids",
+                    "link" => "#"
+                ],
+                [
+                    "text" => "DC universe",
+                    "link" => "#"
+                ],
+                [
+                    "text" => "DC power visa",
+                    "link" => "#"
+                ]
+            ]
+        ],
+        [
+            "title" => "shop",
+            "links" => [
+                [
+                    "text" => "shop DC",
+                    "link" => "#"
+                ],
+                [
+                    "text" => "shop DC collectibles",
+                    "link" => "#"
+                ]
+            ]
+        ]
+    ],
+    "socialsData" => [
+        [
+            "name" => "Facebook",
+            "imageName" => "footer-facebook.png",
+            "link" => "#"
+        ],
+        [
+            "name" => "Twitter",
+            "imageName" => "footer-twitter.png",
+            "link" => "#"
+        ],
+        [
+            "name" => "Youtube",
+            "imageName" => "footer-youtube.png",
+            "link" => "#"
+        ],
+        [
+            "name" => "Pinterest",
+            "imageName" => "footer-pinterest.png",
+            "link" => "#"
+        ],
+        [
+            "name" => "Periscope",
+            "imageName" => "footer-periscope.png",
+            "link" => "#"
+        ]
+    ]
+];
