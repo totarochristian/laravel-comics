cbd139c21c3e77509c64b504b7dd64ff083ff246
totarochristian <totaro.christian.18@gmail.com>
2023-05-22 18:31:37 +0200

    Add main areas

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+{{-- @section('content')
+    <section class="container">
+        <h1>La Molisana</h1>
+        <ul>
+            @foreach ($products as $product)
+                <li><a href="{{ route('products.show', ['id' => $loop->index]) }}">{{ $product['titolo'] }}</a></li>
+            @endforeach
+        </ul>
+    </section>
+@endsection --}}
---
+++ resources/views/mainAreas/footer.php
@@ -0,0 +1,5 @@
+<footer class="bg-custom-dark d-flex flex-column justify-content-center align-items-center overflow-hidden">
+    <BuyComicsComponent />
+    <SitesComponent />
+    <ContactsComponent />
+</footer>
